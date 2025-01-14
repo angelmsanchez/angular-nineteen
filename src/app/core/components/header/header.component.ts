3072350b6a4f5ddb54ee58a3c4919572cf10042e
Angel Muñoz <tok.amunoz@iberia.es>
2025-01-14 09:26:46 +0100

    feat: add feature change language

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,13 +1,22 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [MatToolbarModule, MatIconModule]
+  imports: [MatToolbarModule, MatIconModule, TranslateModule]
 })
 export class HeaderComponent {
   @Output() setOpenMenu = new EventEmitter<void>();
+
+  constructor(private translateService: TranslateService) {}
+
+  changeLanguage(language: string): void {
+    console.log('changeLanguage', language);
+    this.translateService.use(language);
+    // dayjs.locale(language);
+  }
 }
